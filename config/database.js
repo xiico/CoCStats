91836e4bee20afcc8956125e49ed5feeb2cff034
francisco <francisco.marao@hotmail.com>
2017-07-01 12:26:13 -0300

    achievements layout

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
     'url': 'mongodb://localhost:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
+    //'url': 'mongodb://dbuser:dbuser@ds143990.mlab.com:43990/bridges'
 };
