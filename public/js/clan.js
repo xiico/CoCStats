3f4fc31c108a520d2f8e8b056417a54e5922b9c1
SENAC\francisco.cnmarao <francisco.cnmarao@sp.senac.br>
2017-08-18 14:32:45 -0300

    fix minor chart error

--- public/js/clan.js
+++ public/js/clan.js
@@ -38,19 +38,21 @@ $.ajax({
 
 google.charts.setOnLoadCallback(drawChart);
 function drawChart(historyData) {
-//   var data = google.visualization.arrayToDataTable([
-//     ['Year', 'Sales', /*'Expenses'*/],
-//     ['2004',  1000,     /* 400*/],
-//     ['2005',  1170,     /* 460*/],
-//     ['2006',  660,      /* 1120*/],
-//     ['2007',  1030,     /* 540*/]
-//   ]);
-  var data = google.visualization.arrayToDataTable(historyData);
-  var options = {
-    title: 'Clan Points History',
-    //curveType: 'function',
-    legend: { position: 'bottom' }
-  };
-  var chart = new google.visualization.LineChart(document.getElementById('chart'));
-  chart.draw(data, options);
+    //   var data = google.visualization.arrayToDataTable([
+    //     ['Year', 'Sales', /*'Expenses'*/],
+    //     ['2004',  1000,     /* 400*/],
+    //     ['2005',  1170,     /* 460*/],
+    //     ['2006',  660,      /* 1120*/],
+    //     ['2007',  1030,     /* 540*/]
+    //   ]);
+    if (historyData) {
+        var data = google.visualization.arrayToDataTable(historyData);
+        var options = {
+            title: 'Clan Points History',
+            //curveType: 'function',
+            legend: { position: 'bottom' }
+        };
+        var chart = new google.visualization.LineChart(document.getElementById('chart'));
+        chart.draw(data, options);
+    }
 }
