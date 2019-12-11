94d3840767b216f76b7935763003af8b1d33c938
Francisco Marao <francisco.cnmarao@sp.senac.br>
2019-12-11 10:02:27 -0300

    update packages

--- server.js
+++ server.js
@@ -17,14 +17,19 @@ var session      = require('express-session');
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
-mongoose.connect(configDB.url, { useNewUrlParser: true }); // connect to our database
+mongoose.connect(configDB.url, { useNewUrlParser: true, useUnifiedTopology: true }); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+
+// app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
@@ -32,7 +37,11 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade'); // set up jade for templating
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
